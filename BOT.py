cf723d8d1c380153af5980a10ce843c240dd4ecd
Adityarao2390 <114827045+Adityarao2390@users.noreply.github.com>
2023-02-10 22:10:47 +0530

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,33 +1,74 @@
 import streamlit
-import discord
-#import os
+import pandas
+import requests
+#import snowflake.connector
+from urllib.error import URLError
+streamlit.title('Vegetable and Fruits Mart-sa')
 
-#client = discord.Client()
-client = discord.Client(intents=discord.Intents.default())
+streamlit.header('Vegetable Mart')
+streamlit.text('{Patatos: High')
+streamlit.text('Tomatos: Medium')
+streamlit.text('Onion: Medium')
+#streamlit.text('')
+streamlit.header('Sabji Mart')
+streamlit.text('Califlower: High')
+streamlit.header('Fruits Mart')
+streamlit.header('Apple: High')
+"""
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-@client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # write your own comment - what does this do?
+    return fruityvice_normalized
+ 
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ if not fruit_choice:
+  streamlit.error("Please Select a Fruit to get information")
+# streamlit.write('The user entered ', fruit_choice)
+ else:
+   back_from_fumction=get_fruityvice_data(fruit_choice)
+   streamlit.dataframe(back_from_fumction)
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+except URLError as e:
+ streamlit.error()
+# streamlit.stop()
 
-  if message.content.startswith('Hello'):
-    await message.channel.send('Hello!')
+streamlit.header("View our Fruit List - Add Your Fav !")
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list" )
+    return my_cur.fetchall()
+   
+# button
+if streamlit.button('Get fruit List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+# streamlit.header("The Fruit List Contains:")
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
+# streamlit.stop()
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('"+new_fruit+"')" )
+    return "Thanks for adding "+new_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+# button
+if streamlit.button('Add a Fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
+# streamlit.write('Thanks for adding ', add_my_fruit)
 
-  if message.content.startswith('Who invented you'):
-    await message.channel.send('Adi Bhaya')
-
-  if message.content.startswith('Vegetable'):
-    await message.channel.send('Apple:High, Onion:High, Patao:High')
-  
-
-
-client.run(
-  #os.getenv('TOKEN'))
-  'MTA3MzU5MjgwMDU0NjkzMDgyMA.GbPx1T.b-2LEYaAch6oqkLxfYEI1MfkDdHaXVZdSUy5ug')
-  #'MTA3MzI2NzYyMDk5ODA0MTY0MQ.GqjzAU.9ogjXkHBH_kJV2HhmjarZgT9m-3X5lsCJ9eXA8')
+"""
