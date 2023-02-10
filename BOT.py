e81a2c48fe17a94801147576b5efa7bfc4af3fb7
Adityarao2390 <114827045+Adityarao2390@users.noreply.github.com>
2023-02-10 21:59:31 +0530

    Added

---
+++ BOT.py
@@ -0,0 +1,33 @@
+import streamlit
+import discord
+#import os
+
+#client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
+
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('Hello'):
+    await message.channel.send('Hello!')
+
+  if message.content.startswith('Who invented you'):
+    await message.channel.send('Adi Bhaya')
+
+  if message.content.startswith('Vegetable'):
+    await message.channel.send('Apple:High, Onion:High, Patao:High')
+  
+
+
+client.run(
+  #os.getenv('TOKEN'))
+  'MTA3MzU5MjgwMDU0NjkzMDgyMA.GbPx1T.b-2LEYaAch6oqkLxfYEI1MfkDdHaXVZdSUy5ug')
+  #'MTA3MzI2NzYyMDk5ODA0MTY0MQ.GqjzAU.9ogjXkHBH_kJV2HhmjarZgT9m-3X5lsCJ9eXA8')
