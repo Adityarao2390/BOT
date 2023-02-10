e6b3c32ac5372faf3f9397cd426314822aca951f
Adityarao2390 <114827045+Adityarao2390@users.noreply.github.com>
2023-02-10 22:18:34 +0530

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -2,15 +2,25 @@ import streamlit
 import pandas
 import requests
 #import snowflake.connector
-from urllib.error import URLError
+#from urllib.error import URLError
 streamlit.title('Vegetable and Fruits Mart-sa')
 
 streamlit.header('Vegetable Mart')
-streamlit.text('{Patatos: High')
-streamlit.text('Tomatos: Medium')
+streamlit.text('Patatos: High')
+streamlit.text('Tomatos: Medium ')
 streamlit.text('Onion: Medium')
+streamlit.text('Capsicum Medium')
+
 #streamlit.text('')
 streamlit.header('Sabji Mart')
 streamlit.text('Califlower: High')
+streamlit.header('Main Cource Mart')
+streamlit.text('Jackfruit Mart: High')
+streamlit.text('Palak Mart: Medium')
+
+streamlit.header('Spices Mart')
+streamlit.text('Garlic High')
+
+
 streamlit.header('Fruits Mart')
-streamlit.header('Apple: High')
+streamlit.text('Apple: High')
