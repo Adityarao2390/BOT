555c1e723a2e9d3b9739a31866484deac4273197
Adityarao2390 <114827045+Adityarao2390@users.noreply.github.com>
2023-02-11 07:55:56 +0530

    Update Bot

--- BOT.py
+++ BOT.py
@@ -5,24 +5,24 @@ import requests
 #from urllib.error import URLError
 streamlit.title('Vegetable and Fruits Mart-sa')
 
-streamlit.header('Vegetable Mart')
+streamlit.header('🥔🍅Vegetable Mart')
 streamlit.markdown('Patatos: High')
 streamlit.markdown('Tomatos: Medium ')
 streamlit.markdown('Onion: Medium')
 streamlit.markdown('Capsicum Medium')
 
 #streamlit.text('')
-streamlit.header('Sabji Mart')
+streamlit.header('🥘Sabji Mart')
 streamlit.markdown('Califlower: High')
-streamlit.header('Main Cource Mart')
+streamlit.header('♨️Main Cource Mart')
 streamlit.markdown('Jackfruit: High')
 streamlit.markdown('Palak: Medium')
 
-streamlit.header('Spices Mart')
+streamlit.header('🌶🧄Spices Mart')
 streamlit.markdown('Garlic High')
 
 
-streamlit.header('Fruits Mart')
+streamlit.header('🍎Fruits Mart')
 streamlit.markdown('Apple: High')
 
 streamlit.sidebar.radio('Select one:', [1, 2])
